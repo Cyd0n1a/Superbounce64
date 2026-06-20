acb9dc6500b3c05b2b1cff6e9ea808ee03bee180
Cyd0n1a <Cyd0n1a@users.noreply.github.com>
2026-06-20 09:13:31 +0100

    Update CFLAGS and CXXFLAGS to suppress all warnings.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker run --rm \
     -v "$(pwd)":/project \
     -w /project \
     ghcr.io/dragonminded/libdragon:latest \
-    sh -c 'make -C libdragon install tools-install && make -C tiny3d clean && make -C tiny3d CFLAGS="-Wno-error" CXXFLAGS="-Wno-error" && make "$@"'
+    sh -c 'make -C libdragon install tools-install && make -C tiny3d clean && make -C tiny3d CFLAGS="-Wno-error=all" CXXFLAGS="-Wno-error=all" && make "$@"'
